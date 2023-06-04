a449d8b4962178e140add2df542048f23456511a
crackMaker <aleksa.andric064@gmail.com>
2023-06-04 14:42:10 +0000

    Completed setup.sh, added install_requierments for gum, a pretty tui

--- setup.sh
+++ setup.sh
@@ -1,19 +1,56 @@
 #!/bin/bash
-
 # installs the antimalware software
+# this sudo is here so that you only need to enter your password on startup
+sudo echo "Welcome to whisk, where we whisk the malware away!"
 
+install_all_software(){
+    # installs and updates clam
+    # https://help.ubuntu.com/community/ClamAV 
+    sudo apt install clamav clamav-daemon -y
+    sudo systemctl stop clamav-freshclam
+    sudo freshclam
+    sudo systemctl start clamav-freshclean
+    sudo systemctl enable clamav-freshclean
 
-# installs and updates clam
-# https://help.ubuntu.com/community/ClamAV 
-sudo apt install clamav clamav-daemon
-sudo systemctl stop clamav-freshclam
-sudo freshclam
-sudo systemctl start clamav-freshclean
-sudo systemctl enable clamav-freshclean
+    # installs and updates RKhunter
+    sudo apt install rkhunter -y
+    sudo rkhunter --propupd
 
-# installs and updates RKhunter
-sudo apt install rkhunter
-sudo rkhunter --propupd
+    # installs CHKRootkit
+    sudo apt install chkrootkit -y
+}
 
-# installs CHKRootkit
-sudo apt install chkrootkit
+install_chkrootkit(){
+    # installs CHKRootkit
+    sudo apt install chkrootkit -y
+}
+
+install_RKhunter(){    
+    # installs and updates RKhunter
+    sudo apt install rkhunter
+    sudo rkhunter --propupd
+}
+
+install_clamav(){
+    # installs and updates clam
+    # https://help.ubuntu.com/community/ClamAV 
+    sudo apt install clamav clamav-daemon
+    sudo systemctl stop clamav-freshclam
+    sudo freshclam
+    sudo systemctl start clamav-freshclean
+    sudo systemctl enable clamav-freshclean
+}
+
+choice=$(gum choose "Install all software" "Install chkrootkit" "Install clamav" "Install RKHunter" "Exit")
+
+if [[ $choice == "Install all software" ]]; then
+    install_all_software
+elif [[ $choice == "Install chkrootkit" ]]; then
+    install_chkrootkit
+elif [[ $choice == "Install clamav" ]]; then
+    install_clamav
+elif [[ $choice == "Install RKHunter" ]]; then
+    install_RKhunter
+elif [[ $choice == "Exit" ]]; then
+    exit
+fi
--- temp.sh
+++ temp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo $(gum choose "fix" "beans")
+choice=$(gum choose "fix" "beans")
+
+get_name() {
+  echo "John"
+}
+
+echo "You are $(get_name)"
