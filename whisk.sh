dab91556ef4d03df03510caad59f4fcd8fc9ae45
crackMaker <aleksa.andric064@gmail.com>
2023-06-05 02:17:41 +0200

    run_all_software() now removes previous logs, also added comments

--- whisk.sh
+++ whisk.sh
@@ -5,38 +5,45 @@ gum style --border normal --margin "1" --padding "1 2" --border-foreground 130 "
 echo "Welcome to whisk, where we whisk the malware away!"
 
 run_all_software(){
+    ## remove previous logs
+    rm logs/clam.log logs/rkhunter.log logs/chkrootkit.log
 
+    ## based on the choice made, do
     if [[ $choice2 == "Run and output to a log file" ]]; then
+        ## run all software, output all to logs
         for x in clam rkhunter chkrootkit; do
             fancy_text 1 >> $x.log
         done
-        sudo clamscan -r | tee -a clam.log
-        sudo sudo rkhunter --checkall | tee -a rkhunter.log
-        sudo chkrootkit | tee -a chkrootkit.log
+        sudo clamscan -r | tee -a logs/clam.log
+        sudo sudo rkhunter --checkall | tee -a logs/rkhunter.log
+        sudo chkrootkit | tee -a logs/chkrootkit.log
         for x in clam rkhunter chkrootkit; do
             fancy_text 0 >> $x.log
         done
         exit
 
     elif [[ $choice2 == "Just run" ]]; then
+        ## run all software in current env
         sudo clamscan -r
         sudo sudo rkhunter --checkall
         sudo chkrootkit
         exit
 
     elif [[ $choice2 == "Run in background" ]]; then
+        #run all software in the background
         sudo clamscan -r &
         sudo sudo rkhunter --checkall &
         sudo chkrootkit &
         exit
 
     elif [[ $choice2 == "Run in background and output to file" ]]; then
+        ## run all software in the background, and write their outputs to respective logs
         for x in clam rkhunter chkrootkit; do
             fancy_text 1 >> $x.log
         done
-        sudo clamscan -r >> clam.log &
-        sudo sudo rkhunter --checkall >> rkhunter.log &
-        sudo chkrootkit >> chkrootkit.log &
+        sudo clamscan -r >> logs/clam.log &
+        sudo sudo rkhunter --checkall >> logs/rkhunter.log &
+        sudo chkrootkit >> logs/chkrootkit.log &
         for x in clam rkhunter chkrootkit; do
             fancy_text 0 >> $x.log
         done
