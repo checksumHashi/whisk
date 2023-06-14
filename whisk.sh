01a2eab7d75def76cc537409443f1c50ada6d475
crackMaker <aleksa.andric064@gmail.com>
2023-06-14 19:57:49 +0200

    Removed need for setup.sh

--- whisk.sh
+++ whisk.sh
@@ -4,21 +4,63 @@ sudo echo "."
 gum style --border normal --margin "1" --padding "1 2" --border-foreground 130 "Welcome to $(gum style --foreground 130 'Whisk v0.1'), where we $(gum style --foreground 130 'Whisk') the malware away!"
 echo "Welcome to whisk, where we whisk the malware away!"
 
-run_all_software(){
+run_commands(){
+
+}
+
+install_all_software(){\
+    sudo apt update
+
+    # installs and updates clam
+    # https://help.ubuntu.com/community/ClamAV 
+    sudo apt install clamav clamav-daemon -y
+    sudo systemctl stop clamav-freshclam
+    sudo freshclam
+    sudo systemctl start clamav-freshclean
+    sudo systemctl enable clamav-freshclean
+
+    # installs and updates RKhunter
+    sudo apt install rkhunter -y
+    sudo rkhunter --propupd
+
+    # installs CHKRootkit
+    sudo apt install chkrootkit -y
+
+    echo "ALL SOFTWARE INSTALLED, PLEASE RUN WHISK AGAIN TO SCAN"
+    exit
+}
+
+function run_antivirus_scans {
+    # Run ClamAV scan with default options
+    clamscan -r / >> whisk.log
+
+    # Run Chkrootkit scan with default options
+    chkrootkit >> whisk.log
+
+    # Run Rkhunter scan with default options
+    rkhunter --checkall --skip-keypress >> whisk.log
+
+    # Run AIDE scan with default options
+    aide.wrapper --check >> whisk.log
+    
+  	#Run LMD (Linux Malware Detect) Scan 
+ 	lmd -u -q  && lmd_report="$(lmd report)" && echo "${lmd_report}" | tee -a whisk.log 
+
+  	#Run OSSEC HIDS Scanning 
+	ossec-control start && ossec-logtest > /dev/null && ossec-analysisd	
+}
+
+run(){
     ## remove previous logs
     rm logs/clam.log logs/rkhunter.log logs/chkrootkit.log
 
     ## based on the choice made, do
     if [[ $choice2 == "Run and output to a log file" ]]; then
-        ## Run scanners, output all to logs
-        for x in clam rkhunter chkrootkit; do
-            fancy_text 1 >> $x.log
-        done
         sudo clamscan -r | tee -a logs/clam.log
         sudo sudo rkhunter --checkall | tee -a logs/rkhunter.log
         sudo chkrootkit | tee -a logs/chkrootkit.log
         for x in clam rkhunter chkrootkit; do
-            fancy_text 0 >> $x.log
+            fancy_text 0 >> whisk.log
         done
         exit
 
@@ -37,31 +79,12 @@ run_all_software(){
         exit
 
     elif [[ $choice2 == "Run in background and output to file" ]]; then
-        ## Run scanners in the background, and write their outputs to respective logs
-        for x in clam rkhunter chkrootkit; do
-            fancy_text 1 >> $x.log
-        done
-        sudo clamscan -r >> logs/clam.log &
-        sudo sudo rkhunter --checkall >> logs/rkhunter.log &
-        sudo chkrootkit >> logs/chkrootkit.log &
-        for x in clam rkhunter chkrootkit; do
-            fancy_text 0 >> $x.log
-        done
+        sudo clamscan -r >> whisk.log &
+        sudo sudo rkhunter --checkall >> whisk.log &
+        sudo chkrootkit >> whisk.log &
         exit
     fi
     
-}
-
-run_clamav(){
-    if [[ $choice2 == "Run and output to a log file" ]]; then
-        sudo clamscan -r | tee clam.log
-    elif [[ $choice2 == "Just run" ]]; then
-        sudo clamscan -r
-    elif [[ $choice2 == "Run in background" ]]; then
-        sudo clamscan -r &
-    elif [[ $choice2 == "Run in background and output to file" ]]; then
-        sudo clamscan -r >> clam.log &
-    fi
 }
 
 fancy_text(){
@@ -72,10 +95,12 @@ fancy_text(){
     fi
 }
 
-echo "Choose which software you would like to run"
-choice=$(gum choose "Run scanners"  "Exit")
+echo "Main menu"
+choice=$(gum choose "Run scanners" "Install software" "Exit")
 
-if [[ $choice == "Exit" ]]; then
+if [[ $choice == "Install software" ]]then
+    install_all_software
+elif [[ $choice == "Exit" ]]; then
     exit
 fi
 
